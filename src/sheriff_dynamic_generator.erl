4887b58132906776f5ebc9adf4fa70650723b3b0
M <m@m-laptop.(none)>
2011-09-03 18:05:29 +0200

    sheriff_dynamic_generator: add atom,integer..
    You can now make call like:
    sheriff:check(Var,my_type(1,a)).

--- src/sheriff_dynamic_generator.erl
+++ src/sheriff_dynamic_generator.erl
@@ -1,2 +1,15 @@
 -module(sheriff_dynamic_generator).
 -compile(export_all).
+
+% specific value of: atom() , integer() , float() , string()
+% add negative numbers
+find_f(Param,{atom,_L,Val})->
+    Param=:=Val;
+find_f(Param,{integer,_L,Val})->
+    Param=:=Val;
+find_f(Param,{float,_L,Val})->
+    Param=:=Val;
+find_f(Param,{string,_L,Val})->
+    Param=:=Val;
+
+find_f(_,{var,_L,'_'})->true;
