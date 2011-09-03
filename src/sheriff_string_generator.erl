6c9d15a5b7c2f9602fdc4356e42b3bbe325e2c37
M <m@m-laptop.(none)>
2011-09-03 20:06:53 +0200

    sheriff_string_genrator: add a function that register user's type definition

--- src/sheriff_string_generator.erl
+++ src/sheriff_string_generator.erl
@@ -13,3 +13,9 @@
 database()->
     ets:new(my_table, [named_table, protected, set, {keypos, 1}]),
     ets:insert(my_table, {'sheriff_$_var', 0}).
+
+%% It registers types defined by the user within the module, for checking 
+%% purpose.
+-spec register_type({atom(),integer(),[any()]})->no_return().
+register_type({Type_name,_,List_of_type_arg})->
+    ets:insert(my_table, {Type_name, length(List_of_type_arg)}).
