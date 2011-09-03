607200d5beb4bd6c283fbb9e286903d19ed78969
M <m@m-laptop.(none)>
2011-09-03 18:13:42 +0200

    sheriff_dynamic_generator: you can use range (-5..10)
    YOu can now make call like:
    sheriff:check(Var,my_type(-7..11)).

--- src/sheriff_dynamic_generator.erl
+++ src/sheriff_dynamic_generator.erl
@@ -13,3 +13,15 @@ find_f(Param,{string,_L,Val})->
     Param=:=Val;
 
 find_f(_,{var,_L,'_'})->true;
+
+% range  (ex: -10..10) 
+find_f(Param,{type,_,range,[{integer,_,Deb},{integer,_,Fin}]})->
+    is_integer(Param) andalso (Deb=<Param) andalso (Param=<Fin);
+find_f(Param,{type,_,range,[{op,_,'-',{integer,_,Deb}},{integer,_,Fin}]})->
+    is_integer(Param) andalso (Param=<Deb) andalso (Param=<Fin);
+find_f(Param,{type,_,range,[{integer,_,Deb},{op,_,'-',{integer,_,Fin}}]})->
+    is_integer(Param) andalso (Deb=<Param) andalso (Fin=<Param);
+find_f(Param,{type,_,range,[{op,_,'-',{integer,_,Deb}},
+                                            {op,_,'-',{integer,_,Fin}}]})->
+    is_integer(Param) andalso (Param=<Deb) andalso (Fin=<Param);
+
