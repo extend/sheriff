80d59cbddf09332de8860c980c8f027501373e07
M <m@m-laptop.(none)>
2011-09-10 19:12:39 +0200

    sheriff_static_generator: build-in type added

--- src/sheriff_static_generator.erl
+++ src/sheriff_static_generator.erl
@@ -44,6 +44,25 @@ build_f({attribute,_L,type,{Type_name,Type_def,List_of_type_arg}})->
 %% @doc any()
 build_f(_,{var,_L,'_'},_)->{atom,1,true};
 
+%% @doc none() might be removed
+build_f(_,{type,_L,none,[]},_)->{atom,1,false};
+
+%% @doc pid()
+build_f(Param,{type,_L,pid,[]},_)->
+    {call,1,{atom,1,is_pid},[{var,1,Param}]};
+
+%% @doc port()
+build_f(Param,{type,_L,port,[]},_)->
+    {call,1,{atom,1,is_port},[{var,1,Param}]};
+
+%% @doc reference()
+build_f(Param,{type,_L,reference,[]},_)->
+    {call,1,{atom,1,is_reference},[{var,1,Param}]};
+
+%% @doc [] 
+build_f(Param,{type,_L,nil,[]},_)->
+    {op,1,'=:=',{var,1,Param},{nil,1}};
+
 %% @doc Atom : atom() | Erlang_Atom
 % atom()
 build_f(Param,{type,_L,atom,[]},_)->
@@ -52,15 +71,34 @@ build_f(Param,{type,_L,atom,[]},_)->
 build_f(Param,{atom,_L,Val},_)->
     {op,1,'=:=',{var,1,Param},{atom,1,Val}};
 
-%% @doc Binary : binary() 
+%% @doc Binary : binary() |<<>>|<<_:Rem>>|<<_:_*Div>>|<<_:Rem , _:_*Div>>
+% binary()
 build_f(Param,{type,_L,binary,[]},_)->
     {call,1,{atom,1,is_binary},[{var,1,Param}]};
+% <<>>|<<_:Rem>>|<<_:_*Div>>|<<_:Rem , _:_*Div>>
+build_f(Param,{type,_L,binary,[{integer,_,Rem},{integer,_,Div}]},_)->
+    {op,1,'andalso',
+        {call,1,{atom,1,is_bitstring},[{var,1,Param}]},
+	{op,1,'==',
+            {op,1,'rem',
+                {call,1,{atom,1,bit_size},[{var,1,Param}]},
+                {integer,1,Div}
+            },
+            {integer,1,Rem}
+        }
+    };
 
 %% @doc float()
 build_f(Param,{type,_L,float,[]},_)->
     {call,1,{atom,1,is_float},[{var,1,Param}]};
 
 %% @doc Fun : fun()
+%% @TODO fun are tested in a generical way, add:
+%       fun((...)->integer())|fun(()->integer())
+%           |fun((atom(),float())->integer()|float()).
+% fun()
+build_f(Param,{type,_L,'fun',_},_)->
+    {call,1,{atom,1,is_function},[{var,1,Param}]};
 
 %% @doc Integer : integer() | Erlang_Integer | Erlang_Integer..Erlang_Integer
 % integer()
@@ -116,6 +154,8 @@ build_f(Param,{type,_L,list,[Type_def]},List_of_type_arg)->
 		        ]}]}},
 	        {var,1,Param}]}]}]}},
         []}};
+%% @TODO handel then dynamically
+% improper_list(integer(),atom()) | maybe_improper_list(integer(),atom())
 
 %% @doc Tuple :: tuple() | {} | {TList}
 % tuple()
@@ -156,8 +196,116 @@ build_f(Param,{type,_L,union,[H|T]},List_of_type_arg)->
         build_f(Param,H,List_of_type_arg),
         build_f(Param,{type,_L,union,T},List_of_type_arg)
     };
+%% @doc UserDefined <--see in the end, after alias
 
-%% @doc UserDefined type: it handel type's parameters value
+%% @doc [T,...] (the "nonempty_list")
+build_f(Param,{type,_L,nonempty_list,Type_def},List_of_type_arg)->
+    {op,1,'andalso',
+        build_f(Param,{type,_L,list,Type_def},List_of_type_arg),
+        {op,1,'/=',
+	    {call,1,{atom,1,length},[{var,1,Param}]},
+            {integer,1,0}
+        }
+    };
+
+
+%% ===========================================
+%% @doc Built-in type / Alias
+
+% term()
+build_f(_,{type,_L,term,[]},_)->{atom,1,true};
+
+% boolean()
+build_f(Param,{type,_L,boolean,[]},_)->
+    {op,1,'orelse',
+        {op,1,'/=',{var,1,Param},{atom,1,true}},
+        {op,1,'/=',{var,1,Param},{atom,1,false}}
+    };
+
+% byte()
+build_f(Param,{type,_L,byte,[]},_V)->
+    build_f(Param,{type,1,range,[{integer,1,0},{integer,1,255}]},_V);
+
+% char()
+build_f(Param,{type,_L,char,[]},_V)->
+    build_f(Param,{type,1,range,[{integer,1,0},{integer,1,1114111}]},_V);
+
+% non_neg_integer()
+build_f(Param,{type,_L,non_neg_integer,[]},_V)->
+    {op,1,'andalso',
+	{call,1,{atom,1,is_integer},[{var,1,Param}]},
+	{op,1,'>=',{var,1,Param},{integer,1,0}}
+    };
+
+% pos_integer()
+build_f(Param,{type,_L,pos_integer,[]},_V)->
+    {op,1,'andalso',
+	{call,1,{atom,1,is_integer},[{var,1,Param}]},
+	{op,1,'>',{var,1,Param},{integer,1,0}}
+    };
+
+% neg_integer()
+build_f(Param,{type,_L,neg_integer,[]},_V)->
+    {op,1,'andalso',
+        {call,1,{atom,1,is_integer},[{var,1,Param}]},
+	{op,1,'<',{var,1,Param},{integer,1,0}}
+    };
+
+% number()
+build_f(Param,{type,_L,number,[]},_V)->
+    {op,1,'orelse',
+        {call,1,{atom,1,is_integer},[{var,1,Param}]},
+        {call,1,{atom,1,is_float},[{var,1,Param}]}
+    };
+
+% list() : see List
+
+%% @TODO handel dynamically
+% maybe_improper_list()
+% maybe_improper_list(T)
+
+% string()
+build_f(Param,{type,_L,string,[]},_V)->
+    build_f(Param,{type,_L,list,[{type,_L,char,[]}] },_V);
+
+% nonempty_string()
+build_f(Param,{type,_L,nonempty_string,[]},_V)->
+    build_f(Param,{type,_L,nonempty_list,[{type,_L,char,[]}] },_V);
+
+%% @TODO handel dynamically iolist()
+
+% module()
+build_f(Param,{type,_L,module,[]},_V)->
+    build_f(Param,{type,_L,atom,[]},_V);
+
+% mfa()
+build_f(Param,{type,_L,mfa,[]},_V)->
+    build_f(Param,{type,_L,tuple,
+        [{type,22,atom,[]},{type,22,atom,[]},{type,22,byte,[]}] 
+                    },_V);
+
+% node()
+build_f(Param,{type,_L,node,[]},_V)->
+    build_f(Param,{type,_L,atom,[]},_V);
+
+% timeout()
+build_f(Param,{type,_L,timeout,[]},_V)->
+    {op,1,'orelse',
+        build_f(Param,{atom,_L,infinity},_V),
+        build_f(Param,{type,_L,non_neg_integer,[]},_V)
+    };
+
+% no_return()
+build_f(Param,{type,_L,no_return,[]},_V)->
+    build_f(Param,{type,_L,none,[]},_V);
+
+%% ===========================================
+
+% for record
+build_f(Param,{type,_L,record,[{atom,_,Record_name}] },_V)->
+    {call,1,{atom,1,is_record},[{var,1,Param},{atom,1,Record_name}]};
+
+%% @doc For UserDefined type: it handel type's parameters value
 build_f(Param,{var,_L,Val},_)->
     {call,1,
         {remote,1,{atom,1,sheriff_dynamic_generator},{atom,1,find_f}},
@@ -175,7 +323,7 @@ build_f(Param,{type,_L,Type_name,Type_param},_)->
         false-> error("undifined type / not supported yet")
     end;
 
-% @doc For UserDefined type:
+% @doc UserDefined type:
 % In order to use the type defined by the user, which are in other modules
 % NOTE:
 % -these modules should have been compile using the {parse_transform,sheriff}
